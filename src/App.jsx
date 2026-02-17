0ae2fd7f962cf23da18072fc9affde4659e0ffaa
pawan <pawanjalandhara@gmail.com>
2026-02-17 13:01:31 +0530

    update_08

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,84 @@
-import { useEffect, useState } from "react";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithPopup,
-  signOut,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { app } from "./firebase";
+import React, { useState,useEffect } from 'react'
+import { getFirestore, collection, addDoc ,doc,getDoc,} from 'firebase/firestore'
+import { app } from './firebase'
 
-const auth = getAuth(app);
-const GoogleProvider = new GoogleAuthProvider();
+const firestore = getFirestore(app);
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [isSignin, setIsSignin] = useState(false); // toggle signup/signin
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  /* ================= GOOGLE ================= */
-  const signupWithGoogle = async () => {
+const [user_data,setuser_data]=useState('')
+  const writeData = async () => {
     try {
-      await signInWithPopup(auth, GoogleProvider);
-    } catch (error) {
-      console.log(error.message);
+      const result = await addDoc(collection(firestore, 'cities'), {
+        name: 'Delhi',
+        pincode: 1234,
+        lat: 123,
+        long: 456,
+      });
+setuser_data(result)
+      console.log('result', result);
+    } catch (err) {
+      console.error('Error:', err);
     }
-  };
-
-  /* ================= EMAIL SIGNUP ================= */
-  const handleSignup = async () => {
+  }
+   const makeSubCollection= async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      alert("Signup Successful");
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+      const result = await addDoc(collection(firestore, 'cities/5fDtvyfBwoemCC7bYrPm/places'), {
+        name: 'place',
+       desc:'awsm desc',
+       date:Date.now()
+      });
 
-  /* ================= EMAIL SIGNIN ================= */
-  const handleSignin = async () => {
+      console.log('result', result);
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  }
+const GetDocument = async () => {
   try {
-    await signInWithEmailAndPassword(auth, email, password);
-    alert("Signin Successful");
-  } catch (error) {
-    if (error.code === "auth/wrong-password") {
-      alert("Wrong password");
-    } else if (error.code === "auth/user-not-found") {
-      alert("User not found. Please signup first.");
-    } else if (error.code === "auth/invalid-credential") {
-      alert("This email is linked with Google. Please login with Google.");
-    } else {
-      alert(error.message);
-    }
-  }
-};
-
-
-  /* ================= AUTH STATE ================= */
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, []);
-
-  /* ================= AUTH PAGE ================= */
-  if (!user) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-white shadow-xl rounded-2xl p-8 w-[380px]">
-
-          <h1 className="text-2xl font-bold text-center mb-4 text-blue-600">
-            {isSignin ? "Signin" : "Signup"}
-          </h1>
-
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full border p-2 mb-3 rounded"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full border p-2 mb-4 rounded"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          {/* BUTTON */}
-          <button
-            onClick={isSignin ? handleSignin : handleSignup}
-            className="w-full bg-blue-600 text-white p-2 rounded mb-3"
-          >
-            {isSignin ? "Signin" : "Create Account"}
-          </button>
-
-          {/* GOOGLE */}
-          <button
-            onClick={signupWithGoogle}
-            className="w-full border p-2 rounded mb-3"
-          >
-            Continue with Google
-          </button>
-
-          {/* TOGGLE */}
-          <p className="text-sm text-center">
-            {isSignin ? "Don't have account?" : "Already have account?"}
-            <span
-              className="text-blue-600 cursor-pointer ml-1"
-              onClick={() => setIsSignin(!isSignin)}
-            >
-              {isSignin ? "Signup" : "Signin"}
-            </span>
-          </p>
-        </div>
-      </div>
+    const ref = doc(
+      firestore,
+      'cities',
+      '5fDtvyfBwoemCC7bYrPm',
+      'places',
+      '8NYk1G65WAVmW14Wrm0z'
     );
+
+    console.log("Doc Ref:", ref);
+
+    const snap = await getDoc(ref);
+    if (snap.exists()) {
+      console.log("Document Data:", snap.data());
+            setuser_data(snap.data());
+
+    } else {
+      console.log("No such document found!");
+    }
+  } catch (error) {
+    console.log("Error getting document:", error);
   }
-
-  /* ================= DASHBOARD ================= */
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow text-center">
-        <h1 className="text-xl font-bold mb-2">Dashboard</h1>
-        <p className="mb-4">{user.email}</p>
-
-        <button
-          onClick={() => signOut(auth)}
-          className="bg-red-500 text-white px-4 py-2 rounded"
-        >
-          Logout
-        </button>
-      </div>
-    </div>
-  );
 };
 
-export default App;
+useEffect(() => {
+GetDocument()
+}, [])
+
+  return (
+    <div>
+      <h1>Firebase Firestore</h1>
+        <p>user: {user_data?.name}</p>
+      <p>desc: {user_data?.desc}</p>
+      <p>date: {user_data?.date}</p>
+      <button onClick={writeData} className='bg-black text-white p-2 m-2 rounded'>
+        Write Data
+      </button>
+      <button onClick={makeSubCollection} className='bg-black text-white p-2 m-2 rounded'>
+        Write sub-data
+      </button>
+         <button onClick={GetDocument} className='bg-black text-white p-2 m-2 rounded'>
+       Get-document
+      </button>
+    </div>
+  )
+}
+
+export default App
